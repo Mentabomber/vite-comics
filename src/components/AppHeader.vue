38a6c59d1e57f8953f1fa546aa3a22bab668cb6b
Menta <simcictilen.gmail.com>
2023-05-31 16:18:08 +0200

    macro header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,124 @@
 <script>
 export default {
-    name: "AppHeader"
-      
+    name: "AppHeader",
+    data(){
+        return {
+            links: [
+                {
+                   text: "CHARACTERS",
+                   url: "#",
+                   current: false 
+                },
+                {
+                   text: "COMICS",
+                   url: "#",
+                   current: true
+                },
+                {
+                   text: "MOVIES",
+                   url: "#",
+                   current: false 
+                },
+                {
+                   text: "TV",
+                   url: "#",
+                   current: false 
+                },
+                {
+                   text: "GAMES",
+                   url: "#",
+                   current: false 
+                },
+                {
+                   text: "COLLECTIBLES",
+                   url: "#",
+                   current: false 
+                },
+                {
+                   text: "VIDEOS",
+                   url: "#",
+                   current: false 
+                },
+                {
+                   text: "FANS",
+                   url: "#",
+                   current: false 
+                },
+                {
+                   text: "NEWS",
+                   url: "#",
+                   current: false 
+                },
+                {
+                   text: "SHOP",
+                   url: "#",
+                   current: false 
+                },
+            ]
+        }
+    },
+    methods: { 
+        getImmageURL: function(imgPath){
+            return new URL (imgPath, import.meta.url).href;
+        }
+    }
+   
      
 }
 
 </script>
 
 <template>
+    <header>
+        <nav>
+            <!-- div logo -->
+            <div>
+                <img :src="getImmageURL(`../assets/images/dc-logo.png`)" alt="logo-dc">
+            </div>
 
+            <!-- div lista navbar -->
+            <div>
+            
+                <ul>
+                    <li v-for="link in links">
+                        {{ link.text }}
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+    header nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 150px;
+        border: 1px solid black;
+    }
+
+    div {
+        ul {
+            display: flex;
+            list-style-type: none;
+            align-items: center;
+            padding-top: 11%;   
+            // padding top cosi e anche padding bottom 50px nei li per far stare il bordo sotto al confine con il main
+            
+            li {
+                padding-right: 10px;
+                padding-bottom: 50px;
+                
+
+                &.active,
+                &:hover{
+                    color: rgb(13,110,253);
+                    border-bottom: 3px solid rgb(13,110,253);
+                                  }
+            }
+        }
+    }
+    
 
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,9 +8,19 @@ export default {
 </script>
 
 <template>
-
+    <main>
+        <h1>io sono il main!!!</h1>
+    </main>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+    main{
+        height: 100px;
+        width: 100%;
+        background-color: black;
 
+        h1{
+            color: white;
+        }
+    }
 </style>
